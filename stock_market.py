1647e664fa3349cb5eff1bf32875a08661fb76a5
Shreya Sivakumar <73153068+svshreya02@users.noreply.github.com>
2024-06-10 12:09:07 +0530

    Add files via upload

---
+++ stock_market.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 17:55:08 2019
+
+@author: trainee
+"""
+
+# ## Import the Libraries
+
+# Data Manupulation
+import numpy as np
+import pandas as pd
+
+# Techinical Indicators
+import talib as ta
+
+# Plotting graphs
+import matplotlib.pyplot as plt
+
+# Machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+
+# Data fetching
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override()
+
+
+# ## Import the data
+df = pdr.get_data_yahoo('^NSEI', '2000-01-01', '2018-01-01')
+df = df.dropna()
+df = df.iloc[:,:4]
+df.head()
+
+
+# ## Create Indicators
+df['S_10'] = df['Close'].rolling(window=10).mean()
+df['Corr'] = df['Close'].rolling(window=10).corr(df['S_10'])
+df['RSI'] = ta.RSI(np.array(df['Close']), timeperiod =10)
+df['Open-Close'] = df['Open'] - df['Close'].shift(1)
+df['Open-Open'] = df['Open'] - df['Open'].shift(1)
+df = df.dropna()
+X = df.iloc[:,:9]
+y = np.where(df['Close'].shift(-1) > df['Close'],1,-1)
+
+# ## Logistic Regression
+
+# ### Split the Dataset and Instantiate Logistic Regression
+split = int(0.7*len(df))
+X_train, X_test, y_train, y_test = X[:split], X[split:], y[:split], y[split:]
+
+model = LogisticRegression()
+model = model.fit (X_train,y_train)
+pd.DataFrame(zip(X.columns, np.transpose(model.coef_)))
+
+probability = model.predict_proba(X_test)
+print(probability)
+
+predicted = model.predict(X_test)
+
+print(metrics.confusion_matrix(y_test, predicted))
+
+print(metrics.classification_report(y_test, predicted))
+
+print(model.score(X_test,y_test))  
+
+cross_val = cross_val_score(LogisticRegression(), X, y, scoring='accuracy', cv=10)
+print(cross_val)
+print(cross_val.mean())
+
+
+# ## Create a Trading Strategy
+df['Predicted_Signal'] = model.predict(X)
+df['Nifty_returns'] = np.log(df['Close']/df['Close'].shift(1))
+Cumulative_Nifty_returns = np.cumsum(df[split:]['Nifty_returns'])
+
+df['Startegy_returns'] = df['Nifty_returns']* df['Predicted_Signal'].shift(1)
+Cumulative_Strategy_returns = np.cumsum(df[split:]['Startegy_returns'])
+
+plt.figure(figsize=(10,5))
+plt.plot(Cumulative_Nifty_returns, color='r',label = 'Nifty Returns')
+plt.plot(Cumulative_Strategy_returns, color='g', label = 'Strategy Returns')
+plt.legend()
+plt.show()
